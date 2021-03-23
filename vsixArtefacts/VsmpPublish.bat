274b6c5b2632d994a63956ca6f53b1bb6efb67c8
GregTrevellick <gtrevellick@googlemail.com>
2021-03-23 21:00:59 +0000

    .

--- vsixArtefacts/VsmpPublish.bat
+++ vsixArtefacts/VsmpPublish.bat
@@ -1,5 +1,6 @@
 # https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2017
 
-cd D:\_Dgit\_MINE_ACTIVE\CleanArchitecture\vsixArtefacts\packages\Microsoft.VSSDK.BuildTools.16.4.1057\tools\vssdk\bin
+#cd D:\_Dgit\_MINE_ACTIVE\CleanArchitecture\vsixArtefacts\packages\Microsoft.VSSDK.BuildTools.16.4.1057\tools\vssdk\bin
+cd C:\_git\CleanArchitecture\vsixArtefacts\packages\Microsoft.VSSDK.BuildTools.16.4.1057\tools\vssdk\bin
 
 .\VsixPublisher.exe publish  -payload "..\..\..\..\..\..\vsixTemplatePackSideWaffle\bin\debug\vsixTemplatePackSideWaffle.vsix"  -publishManifest "..\..\..\..\..\..\vsixArtefacts\VsmpPublish.json"  -personalAccessToken "vsmp_pat"
