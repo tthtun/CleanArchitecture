83a03da06376250db585923f14dc93925510cbc5
GregTrevellick <gtrevellick@googlemail.com>
2021-03-23 18:55:12 +0000

    delete sln src tests

--- src/CleanArchitecture.SharedKernel/BaseDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CleanArchitecture.SharedKernel
-{
-    public abstract class BaseDomainEvent
-    {
-        public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
-    }
-}
